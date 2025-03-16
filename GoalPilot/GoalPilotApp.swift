5dd13a5c8af6ea1d1aeb8fb8560ad38b52cf1432
Rune Pollet <runepollet@icloud.com>
2025-03-16 17:33:11 +0100

    Initial Commit

---
+++ GoalPilot/GoalPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalPilotApp.swift
+//  GoalPilot
+//
+//  Created by Rune Pollet on 16/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct GoalPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
